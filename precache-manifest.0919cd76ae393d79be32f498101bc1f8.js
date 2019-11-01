cbb548e3f19e40cd4c7984ee46fb9204dbfc7c06
Alexander45448 <00098818@uca.edu.sv>
2019-11-01 11:28:45 -0600

    Updates

---
+++ precache-manifest.0919cd76ae393d79be32f498101bc1f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67a8c8b217b33a8e721377be6d6ecb22",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "94997cfce9b2f86c7dfb",
+    "url": "/tictactoe/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "5fc33aed0e7becc2a37c",
+    "url": "/tictactoe/static/js/2.52dd166e.chunk.js"
+  },
+  {
+    "revision": "94997cfce9b2f86c7dfb",
+    "url": "/tictactoe/static/js/main.f084fdea.chunk.js"
+  },
+  {
+    "revision": "1dd1878a39f419eb6036",
+    "url": "/tictactoe/static/js/runtime-main.868a150c.js"
+  }
+]);
